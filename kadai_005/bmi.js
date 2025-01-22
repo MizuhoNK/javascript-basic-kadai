63540b3ee2912497af5b521d8d83594218f6f8fb
MizuhoNK <mizuho.ymd2979@gmail.com>
2025-01-22 22:03:58 +0900

    指摘事項の修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 //変数の宣言
 let weight;
 let height;
+let bmi;
 
 //値の代入
 weight = 68;
 height = 1.7;
 
 //BMI値の計算・出力
-console.log(weight / (height * height));
+bmi = weight / (height * height);
+console.log(bmi);
