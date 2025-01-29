eb40d84769b15010c5c2a45de64e8f8dea114feb
MizuhoNK <mizuho.ymd2979@gmail.com>
2025-01-29 21:32:38 +0900

    指摘事項の修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,11 @@
+//本日の日付を取得
 const today = new Date();
+//本日の日付の年を取得
 const todayYear = today.getFullYear();
-const todayMonth = today.getMonth();
+//本日の日付の月を取得
+//戻り値が0～11なので1を足す
+const todayMonth = today.getMonth() + 1;
+//本日の日付の日を取得
 const todayDay = today.getDate();
+//本日の日付を「〇年〇月〇日」で表示する
 console.log(todayYear + "年" + todayMonth + "月" + todayDay + "日");
